be2ade2a083a15de190e87198ab226ebf38c0520
JhonMarques <jhonatasbcc@gmail.com>
2021-07-21 00:05:39 -0300

    update categorias implementado

--- src/app/components/views/categoria/categoria-update/categoria-update.component.ts
+++ src/app/components/views/categoria/categoria-update/categoria-update.component.ts
@@ -1,22 +1,51 @@
-import { Router } from '@angular/router';
+import { CategoriaService } from './../categoria.service';
+import { Categoria } from './../categoria.model';
+import { Router, ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-categoria-update',
-  templateUrl: "./categoria-update.component.html",
-  styleUrls: ["./categoria-update.component.css"],
-  styles: [
-  ]
+  templateUrl: './categoria-update.component.html',
+  styleUrls: ['./categoria-update.component.css'],
+  styles: [],
 })
 export class CategoriaUpdateComponent implements OnInit {
+  categoria: Categoria = {
+    id: '',
+    nome: '',
+    descricao: '',
+  };
 
-  constructor(private router: Router) { }
+  constructor(
+    private service: CategoriaService, 
+    private router: Router, 
+    private route: ActivatedRoute
+    ) {}
 
   ngOnInit(): void {
+    this.categoria.id = this.route.snapshot.paramMap.get('id')!;
+    this.findById();
   }
 
   cancel(): void {
-    this.router.navigate(['categorias'])
+    this.router.navigate(['categorias']);
   }
 
-}
+  findById(): void {
+    this.service.findById(this.categoria.id!).subscribe(resposta => {
+      this.categoria.nome = resposta.nome;
+      this.categoria.descricao = resposta.descricao;
+    });
+  }
+
+  update(): void {
+    this.service.update(this.categoria).subscribe((resposta) => {
+      this.router.navigate(["categorias"]);
+      this.service.mensagem("Categoria atualizada com sucesso");
+    }, err => {
+      this.service.mensagem('Validar se todos os campos estão preenchidos corretamente!')
+    });
+  }
+  }
+  
+
